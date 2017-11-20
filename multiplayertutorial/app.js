cdbab0416e1500f343e6bb0167f1b11b32b0aeb2
JoSuzuki <jonathan@suzuki.pro.br>
2017-11-19 22:55:20 -0200

    Moving lapras, in two different tabs

--- multiplayertutorial/app.js
+++ multiplayertutorial/app.js
@@ -17,22 +17,95 @@ app.get('/',function(req, res) {
 app.use('/client',express.static(__dirname + '/client'));
 
 //listen on port 2000
-serv.listen(process.env.PORT || 2000);
+serv.listen(process.env.PORT || 2000, "0.0.0.0");
 console.log("Server started.");
 
 var player_lst = [];
 
 var Player = function(startX, startY, startAngle){
-  var x = startX
-  var y = startY
-  var angle = startAngle
+  this.x = startX
+  this.y = startY
+  this.angle = startAngle
 }
 
 function onNewPlayer(data) {
-  var newPlayer = new Player(data.x,data.y,data.angle);
+  var newPlayer = new Player(data.x, data.y, data.angle);
   console.log("Created new player with id " + this.id);
-  player_lst.push(newPlayer);
+	newPlayer.id = this.id;
+	console.log("newPlayer.id = " + newPlayer.id);
 
+	var current_info = {
+		id: newPlayer.id,
+		x: newPlayer.x,
+		y: newPlayer.y,
+		angle: newPlayer.angle,
+	};
+
+	// send to the new player about everyone who is already connected
+	for (i = 0; i < player_lst.length; i++){
+		existingPlayer = player_lst[i];
+		var player_info = {
+			id: existingPlayer.id,
+			x: existingPlayer.x,
+			y: existingPlayer.y,
+			angle: existingPlayer.angle,
+		};
+		console.log("pushing player with id "+existingPlayer.id);
+		//send message to the sender-client only
+		this.emit("new_enemyPlayer", player_info);
+	}
+
+	//send message to every connected client except the sender
+	this.broadcast.emit('new_enemyPlayer', current_info);
+
+	player_lst.push(newPlayer);
+	//console.log("pushed newplayer, player_lst.length: "+player_lst.length);
+
+}
+
+function onMovePlayer(data){
+	var movePlayer = find_playerid(this.id);
+	//console.log("this id: " + this.id);
+	//console.log("broadcast id: " + movePlayer.id)
+	//console.log("broadcast de x: " + data.x);
+	movePlayer.x = data.x;
+	movePlayer.y = data.y;
+	movePlayer.angle = data.angle;
+
+	var moveplayerData = {
+		id: movePlayer.id,
+		x: movePlayer.x,
+		y: movePlayer.y,
+		angle: movePlayer.angle
+	}
+	//send message to every connected cliente except the sender
+	this.broadcast.emit('enemy_move', moveplayerData);
+
+}
+//call when a client disconnects and tell the clients except sender to remove the disconnected player
+function onClientdisconnect(){
+	console.log('disconnect');
+
+	var removePlayer = find_playerid(this.id);
+	if (removePlayer) {
+		player_lst.splice(player_lst.indexOf(removePlayer), 1);
+	}
+
+	console.log("removing player " + this.id);
+
+	//send message to every connected client except the sender
+	this.broadcast.emit('remove_player', {id:this.id});
+}
+
+function find_playerid(id) {
+	//console.log("player_lst.lenght: "+ player_lst.length);
+	//console.log("player_lst: "+player_lst);
+	for (var i = 0; i < player_lst.length; i++){
+		if (player_lst[i].id == id){
+			return player_lst[i];
+		}
+	}
+	return false;
 }
 
  // binds the serv object we created to socket.io
@@ -40,9 +113,19 @@ var io = require('socket.io')(serv,{});
 
 // listen for a connection request from any client
 io.sockets.on('connection', function(socket){
+
 	console.log("socket connected");
+	socket.on("first_connection", function(){
+		socket.emit('first_connection', {});
+		console.log("server sent connect");
+	});
+
+	//listen for disconnection;
+	socket.on('disconnect', onClientdisconnect);
+
+	//listen for new player
+	socket.on("move_player", onMovePlayer);
 
   socket.on("new_player", onNewPlayer);
-	//output a unique socket.id
-	console.log(socket.id);
+
 });
--- multiplayertutorial/client/main.js
+++ multiplayertutorial/client/main.js
@@ -1,5 +1,6 @@
 var socket; // define a global variable called socket
 socket = io.connect(); // send a connection request to the server
+console.log("send a connection request to the server");
 
 //this is just configuring a screen size to fit the game properly
 //to the browser
@@ -9,6 +10,10 @@ canvas_height = window.innerHeight * window.devicePixelRatio;
 //make a phaser game
 game = new Phaser.Game(canvas_width,canvas_height, Phaser.CANVAS,
  'gameDiv');
+
+var enemies = [];
+var player_created = false;
+
 
 var gameProperties = {
 	//this is the actual game size to determine the boundary of
@@ -29,7 +34,19 @@ function onsocketConnected(){
   console.log("onsocketConnected called");
   createPlayer();
   gameProperties.in_game = true;
-  socket.emit('new_player', {x:0, y:0, angle:0});
+  socket.emit('new_player', {x:50, y:50, angle:0});
+}
+
+function onRemovePlayer(data){
+  var removePlayer = findplayerbyid(data.id);
+  //Player not found
+  if(!removePlayer){
+    console.log('Player not found: ', data.id);
+    return;
+  }
+  removePlayer.player.destroy();
+  enemies.splice(enemies.indexOf(removePlayer), 1);
+
 }
 
 function createPlayer(){
@@ -42,17 +59,60 @@ function createPlayer(){
 	player.animations.add('down', [6, 7, 8], 10, true);
 }
 
+var remote_player = function(id, startx, starty, startAngle){
+  this.x = startx;
+  this.y = starty;
+  //this is the unique socket id. We use it as a unique name for each enemy
+  this.id = id;
+  this.angle = startAngle;
+  this.player = game.add.sprite(this.x, this.y, 'lapras');
+  this.player.animations.add('left', [9, 10, 11], 10, true);
+	this.player.animations.add('right', [3, 4, 5], 10, true);
+	this.player.animations.add('up', [0, 1, 2], 10, true);
+	this.player.animations.add('down', [6, 7, 8], 10, true);
+}
+
+function onNewPlayer(data) {
+  console.log("new enemy id: "+data.id);
+  console.log("new enemy x: "+data.x);
+  var new_enemy = new remote_player(data.id, data.x, data.y, data.angle);
+  enemies.push(new_enemy);
+}
+
+function onEnemyMove(data) {
+  //console.log(data.id);
+  //console.log(enemies);
+  console.log("id recebido: " + data.id);
+  console.log("x recebido: " + data.x);
+  var movePlayer = findplayerbyid(data.id);
+  console.log("id moveplayer: " + movePlayer.id);
+  if(!movePlayer){
+    return;
+  }
+  movePlayer.player.x = data.x;
+  movePlayer.player.y = data.y;
+  console.log("after moving: "+ movePlayer.x);
+
+}
+
+function findplayerbyid(id){
+  for(var i = 0; i < enemies.length; i++){
+    if(enemies[i].id == id){
+      return enemies[i];
+    }
+  }
+}
+
 
 // add the
 main.prototype = {
 	preload: function() {
+    game.stage.disableVisibilityChange = true;
     game.load.spritesheet('lapras', 'client/assets/shiny.png', 40, 40);
     game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
     game.world.setBounds(0, 0, gameProperties.gameWidth,
       gameProperties.gameHeight, false, false, false, false);
     game.physics.startSystem(Phaser.Physics.ARCADE);
-
-
 
   },
 	//this function is fired once when we load the game
@@ -62,11 +122,24 @@ main.prototype = {
 		//listen to the “connect” message from the server. The server
 		//automatically emit a “connect” message when the cleint connets.When
 		//the client connects, call onsocketConnected.
-		socket.on("connect", onsocketConnected);
-    console.log("whytho");
+
+		socket.on("first_connection", onsocketConnected);
+    //onsocketConnected();
+    socket.on("new_enemyPlayer", onNewPlayer);
+
+    socket.on("enemy_move", onEnemyMove);
+
+    socket.on("remove_player", onRemovePlayer);
+
 
 	},
   update: function() {
+    if(!player_created){
+      socket.emit('first_connection', {});
+      player_created = true;
+      console.log("first time");
+    }
+
     if(gameProperties.in_game){
       var cursors = game.input.keyboard.createCursorKeys();
       if((cursors.left.isDown) || (cursors.right.isDown) || (cursors.up.isDown) || (cursors.down.isDown)) {
@@ -104,7 +177,7 @@ main.prototype = {
     		player.body.velocity.y = 0;
     		player.animations.stop();
     	}
-
+      socket.emit('move_player', {x: player.x, y: player.y, angle: player.angle});
     }
 
   }
